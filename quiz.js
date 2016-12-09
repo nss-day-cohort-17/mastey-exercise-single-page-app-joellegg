0b1e3477812cc41c0f6ba2f54d2b50083ed93099
joellegg <joellegg@bellsouth.net>
2016-12-09 15:05:50 -0600

    added bs3 navbar with search form

--- quiz.js
+++ quiz.js
@@ -4,7 +4,7 @@ loadInventory();
 function populatePage (e) {
     // Loop over the inventory and populate the page
     var carList = document.getElementById('usedCarList');
-    inventory = JSON.parse(e.target.responseText)
+    inventory = JSON.parse(e.target.responseText);
     carList.innerHTML = inventory.cars[0].make;
     console.log(inventory);
     // Now that the DOM is loaded, establish all the event listeners needed
