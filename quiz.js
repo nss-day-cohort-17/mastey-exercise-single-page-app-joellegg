f01fa8fd9b536d1d19d41db6b53d7312f32b9c92
joellegg <joellegg@bellsouth.net>
2016-12-09 14:57:56 -0600

    added js functions to load and parse json

--- quiz.js
+++ quiz.js
@@ -1,20 +1,26 @@
 var inventory = [];
 loadInventory();
 
-function populatePage (inventory) {
-  // Loop over the inventory and populate the page
-
-  // Now that the DOM is loaded, establish all the event listeners needed
-  activateEvents();
+function populatePage (e) {
+    // Loop over the inventory and populate the page
+    var carList = document.getElementById('usedCarList');
+    inventory = JSON.parse(e.target.responseText)
+    carList.innerHTML = inventory.cars[0].make;
+    console.log(inventory);
+    // Now that the DOM is loaded, establish all the event listeners needed
+    //activateEvents();
 }
 
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
 
-function loadInventory (callback) {
-  var inventoryLoader = new XMLHttpRequest();
-
-  inventoryLoader.addEventListener("load", function () {
-
-  });
+function loadInventory () {
+    var inventoryLoader = new XMLHttpRequest();
+    inventoryLoader.addEventListener("load", function (e) {
+        // whatever anonymous function does.
+        console.log(this);
+        populatePage(e);
+    });
+    inventoryLoader.open("GET", 'inventory.json');
+    inventoryLoader.send();
 }
