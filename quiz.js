8a4f1405adab7dcd672d3d2048c1e04fabf6fd09
joellegg <joellegg@bellsouth.net>
2016-12-09 13:40:26 -0600

    added bs3, copied js, json from instructions

---
+++ quiz.js
@@ -0,0 +1,20 @@
+var inventory = [];
+loadInventory();
+
+function populatePage (inventory) {
+  // Loop over the inventory and populate the page
+
+  // Now that the DOM is loaded, establish all the event listeners needed
+  activateEvents();
+}
+
+// Load the inventory and send a callback function to be
+// invoked after the process is complete
+
+function loadInventory (callback) {
+  var inventoryLoader = new XMLHttpRequest();
+
+  inventoryLoader.addEventListener("load", function () {
+
+  });
+}
