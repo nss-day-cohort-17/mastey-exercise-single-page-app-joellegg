e8ee66c70b167a7164a87d223471fafa2260d0a3
joellegg <joellegg@bellsouth.net>
2016-12-12 15:20:26 -0600

    added js to load inventory to page, added function to style cards on click

--- quiz.js
+++ quiz.js
@@ -1,13 +1,63 @@
 let inventory = [];
+let autoSection = document.querySelector('.autos');
+
 
 function populatePage (e) {
     // Loop over the inventory and populate the page
     const carList = document.querySelector('.autos');
     inventory = JSON.parse(e.target.responseText);
-    console.log(inventory);
+    //console.log(inventory);
+    // for loop to load all cars
+    autoSection.innerHTML = "";
+    let carInventory = "";
+    for (let i = 0; i < inventory.cars.length; i++) {
+        carInventory +=
+        `<div class="col-md-4 col-sm-6">
+            <div class="card">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${inventory.cars[i].year} ${inventory.cars[i].make} ${inventory.cars[i].model}</li>
+                </ul>
+                <img class="card-img-top" src="${inventory.cars[i].image}">
+                <div class="card-block">
+                    <p class="card-text">${inventory.cars[i].description}</p>
+                    <h5 class="card-title">$${inventory.cars[i].price}</h5>
+                </div>
+            </div>
+        </div>`;
+        // for every 3 cards we need a wrapper div with class of row
+        // if i%3 === 0 close div might need ++i instead modulus
+
+    }
+    autoSection.innerHTML = carInventory;
+    //get number of cards on page
+    //console.log(document.querySelectorAll('.card').length);
+
     // Now that the DOM is loaded, establish all the event listeners needed
-    //activateEvents();
+    activateEvents();
 }
+
+document.querySelector('.card').addEventListener('click', function(e){
+  console.dir(e.currentTarget);
+  // activateEvents(e);
+})
+
+function activateEvents() {
+    // event listener on the cards
+    for (let i = 0; i < document.querySelectorAll('.card').length; i++) {
+        document.querySelectorAll('.card')[i].addEventListener('click', function(e){
+            //if a card is already clicked then remove the class and add it to the one that is clicked
+            if (document.querySelector('.cardClick')) {
+                document.querySelector('.cardClick').classList.remove('cardClick');
+            }
+            // add the class of cardClick to the clicked card
+            e.currentTarget.classList.add("cardClick");
+            // move focus to the description field in the navbar
+            let descriptionField = document.querySelector('.form-control');
+            descriptionField.focus();
+        })
+    }
+    // event listener on the description input, include keyup listener
+};
 
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
