bb37f8c6d6c63608680d4275125be968ce8d717f
joellegg <joellegg@bellsouth.net>
2016-12-11 19:19:58 -0600

    fixed bootstrap cdn issue

--- quiz.js
+++ quiz.js
@@ -1,11 +1,9 @@
 let inventory = [];
-loadInventory();
 
 function populatePage (e) {
     // Loop over the inventory and populate the page
-    const carList = document.querySelector('#usedCarList');
+    const carList = document.querySelector('.autos');
     inventory = JSON.parse(e.target.responseText);
-    carList.innerHTML = inventory.cars[0].make;
     console.log(inventory);
     // Now that the DOM is loaded, establish all the event listeners needed
     //activateEvents();
@@ -13,14 +11,7 @@ function populatePage (e) {
 
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
-
-function loadInventory () {
-    const inventoryLoader = new XMLHttpRequest();
-    inventoryLoader.addEventListener("load", (e) => {
-        // whatever anonymous function does.
-        console.log(this);
-        populatePage(e);
-    });
-    inventoryLoader.open("GET", 'inventory.json');
-    inventoryLoader.send();
-}
+const inventoryLoader = new XMLHttpRequest();
+inventoryLoader.addEventListener('load', populatePage);
+inventoryLoader.open('GET', 'inventory.json');
+inventoryLoader.send();
