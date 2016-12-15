5527fc4a77e19cfc8ae9ac000a11f991a468ce51
joellegg <joellegg@bellsouth.net>
2016-12-14 19:13:28 -0600

    changed js to accept function for color. error on line 113

--- quiz.js
+++ quiz.js
@@ -1,15 +1,27 @@
 let inventory = [];
 let autoSection = document.querySelector('.autos');
 
+//function calls
+loadInventory();
 
+// Load the inventory and send a callback function to be invoked after the process is complete
+function loadInventory() {
+    const inventoryLoader = new XMLHttpRequest();
+    inventoryLoader.addEventListener('load', populatePage);
+    inventoryLoader.open('GET', 'inventory.json');
+    inventoryLoader.send();
+}
+
+// parse JSON and populate page
 function populatePage (e) {
-    // Loop over the inventory and populate the page
-    const carList = document.querySelector('.autos');
     inventory = JSON.parse(e.target.responseText);
-    //console.log(inventory);
-    // for loop to load all cars
+    // Get reference to the autos section of html
+    const carList = document.querySelector('.autos');
+    // empty the autos section
     autoSection.innerHTML = "";
+    // declare car inventory variable
     let carInventory = "";
+    // loop over the inventory and load to page
     for (let i = 0; i < inventory.cars.length; i++) {
         if (i%3 === 0) {
             carInventory += `<div class="row">`;
@@ -30,7 +42,6 @@ function populatePage (e) {
         if (i%3 === 2) {
             carInventory += `</div>`;
         }
-        //console.log("i", i, (inventory.cars.length - 1));
         if (i === (inventory.cars.length - 1)) {
             carInventory += `</div>`
         }
@@ -45,36 +56,53 @@ function populatePage (e) {
 function activateEventListeners() {
     // event listener on the cards
     for (let i = 0; i < document.querySelectorAll('.card').length; i++) {
-        document.querySelectorAll('.card')[i].addEventListener('click', function(e){
-            // if the current card already has the cardClick class then remove it
+        document.querySelectorAll('.card')[i].addEventListener('click', (e) => {
+            // if the current card already has the cardClick class then remove it and background color
             if (e.currentTarget.classList.contains('cardClick')) {
-                console.log("Tis true. It has the class cardClick")
-                e.currentTarget.classList.remove('cardClick');
-            // if another card is clicked then remove the class and add it to the one that is clicked
+                // e.currentTarget.classList.remove('cardClick');
+                resetStyling();
+            // if another card is clicked then remove the class and background color and add it to the one that is clicked
             } else if (document.querySelector('.cardClick')) {
-                document.querySelector('.cardClick').classList.remove('cardClick');
+                resetStyling();
                 e.currentTarget.classList.add("cardClick");
-            // else add the cardClick class to the target
+                changeCardColor(e, "red");
+            // else add the cardClick class to the target and change the background color
             } else {
                 e.currentTarget.classList.add("cardClick");
+                changeCardColor(e, "red");
             }
-
-            // move focus to the description field in the navbar
-            let descriptionField = document.querySelector('.form-control');
-            descriptionField.value = "";
-            descriptionField.focus();
-        })
-    editCardDescription();
-    }
+    });
+    // editCardDescription();
 };
 
+function resetStyling () {
+    // if the
+    if (document.querySelector('.cardClick')) {
+        document.querySelector('.cardClick').classList.remove('cardClick');
+    }
+    for (let i = 0; i < inventory.cars.length; i++) {
+        document.querySelectorAll('.card')[i].style.backgroundColor = '';
+    }
+}
+
+function changeCardColor (e, color) {
+
+    // move focus to the description field in the navbar
+    let descriptionField = document.querySelector('.form-control');
+    descriptionField.value = "";
+    descriptionField.focus();
+    //resetStyling(e);
+    e.currentTarget.style.backgroundColor = color;
+    editCardDescription();
+}
 
 function editCardDescription () {
     // event listener on the description input, include keyup listener
     let newDescription = document.querySelector('.form-control');
+    console.log(newDescription);
 
     // on keyup event change the text
-    newDescription.addEventListener('keyup', function(e) {
+    newDescription.addEventListener('keyup', (e) => {
         let editHighlightedCard = document.querySelector('.cardClick').querySelector('.card-block').querySelector('p');
         if (e.key === "Enter") {
             newDescription.value = "";
@@ -83,11 +111,3 @@ function editCardDescription () {
         }
     });
 }
-
-
-// Load the inventory and send a callback function to be
-// invoked after the process is complete
-const inventoryLoader = new XMLHttpRequest();
-inventoryLoader.addEventListener('load', populatePage);
-inventoryLoader.open('GET', 'inventory.json');
-inventoryLoader.send();
